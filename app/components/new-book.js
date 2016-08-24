b65203360631ba10f11ec429e1bc08b4d03b7f44
christine tacha and james turello <james@>
2016-08-24 11:42:27 -0700

    add new-book component

---
+++ app/components/new-book.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend ({
+  addNewBook: false,
+  actions: {
+    bookFormShow() {
+      this.set('addNewBook', true);
+    },
+
+    save() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        genre: this.get('genre')
+      };
+      this.set('addNewBook', false);
+      this.sendAction('save', params);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend ({
   model(){
     return this.store.findAll('book');
   },
+
+  actions: {
+    save(params) {
+      var newBook = this.store.createRecord('book', params);
+      newBook.save();
+      this.transitionTo('index');
+    }
+  }
 });
